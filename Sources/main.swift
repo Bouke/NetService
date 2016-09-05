b3a1ccca5872a1ea0a613d9e02dc2719adaeea5f
Bouke Haarsma <bouke@haarsma.eu>
2016-09-05 07:34:12 +0200

    UDP DNS lookup using libdispatch

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,11 +1,28 @@
 import Darwin
+import Foundation
+
+class MyDelegate: NSObject, StreamDelegate {
+    func stream(_ aStream: Stream, handle eventCode: Stream.Event) {
+        switch (aStream, eventCode) {
+        case (let stream as InputStream, Stream.Event.hasBytesAvailable):
+            var buffer = Data(count: 1024)
+            buffer.count = buffer.withUnsafeMutableBytes {
+                stream.read($0, maxLength: 1024)
+            }
+            buffer.dump()
+            print(Message(unpack: buffer))
+        default:
+            print(aStream, eventCode)
+        }
+    }
+}
 
 func ntohs(_ value: CUnsignedShort) -> CUnsignedShort {
     return (value << 8) + (value >> 8);
 }
 let htons = ntohs
 
-let query = Message(header: Header(id: 0x1B, response: false, operationCode: .query, authoritativeAnswer: false, truncation: false, recursionDesired: true, recursionAvailable: false, returnCode: .NOERROR), questions: [Question(name: "apple.com", type: .nameServer, internetClass: 1)], answers: [], authorities: [], additional: [])
+let query = Message(header: Header(id: 0x1B, response: false, operationCode: .query, authoritativeAnswer: false, truncation: false, recursionDesired: true, recursionAvailable: false, returnCode: .NOERROR), questions: [Question(name: "apple.com", type: .text, internetClass: 1)], answers: [], authorities: [], additional: [])
 
 let INADDR_ANY = in_addr(s_addr: 0)
 
@@ -13,84 +30,110 @@ let INADDR_ANY = in_addr(s_addr: 0)
 //my_addr.sin_family = sa_family_t(AF_INET)
 //my_addr.sin_addr.s_addr = INADDR_ANY.s_addr
 
-let destination = "10.0.1.1"
+//let host = "::1"
+//let port = "53"
 
-let socketfd = socket(AF_INET, SOCK_DGRAM, 0)
+let socketfd = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
 if(socketfd == 0) {
     abort()
 }
 
-var dest_addr = sockaddr_in()
-dest_addr.sin_family = sa_family_t(AF_INET)
-dest_addr.sin_port = htons(53)
-dest_addr.sin_addr.s_addr = destination.withCString { inet_addr($0) }
+// setup socket using bsd calls. Then convert to Input/Output Stream and use run loops.
 
-import Foundation
+var addr2 = sockaddr_in()
+addr2.sin_family = sa_family_t(AF_INET)
+addr2.sin_port = htons(53)
+inet_pton(AF_INET, "10.0.1.1", &addr2.sin_addr)
 
-//var data = Data(try encodeName("apple.com"))
-//var position = data.startIndex
-//print(decodeName(data, position: &position))
-//print(position)
+let msg = query.pack()
+let addr3 = withUnsafePointer(to: &addr2) {
+    $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+        $0
+    }
+}
 
-// try 2
+let (readStream, writeStream) = { () -> (InputStream, OutputStream) in
+    var reads: Unmanaged<CFReadStream>? = nil
+    var writes: Unmanaged<CFWriteStream>? = nil
+    CFStreamCreatePairWithSocket(kCFAllocatorDefault, socketfd, &reads, &writes)
+    return (reads!.takeRetainedValue() as InputStream, writes!.takeRetainedValue() as OutputStream)
+}()
+
+var size = socklen_t(MemoryLayout<sockaddr_in>.size)
+precondition(connect(socketfd, addr3, size) == 0)
+
+let myDelegate = MyDelegate()
+readStream.open()
+readStream.delegate = myDelegate
+readStream.schedule(in: .main, forMode: .defaultRunLoopMode)
+
+writeStream.open()
+print("write", writeStream.write(msg, maxLength: msg.count))
+
+withExtendedLifetime((myDelegate, readStream)) {
+    RunLoop.main.run()
+    print(myDelegate)
+}
 
 //var info = addrinfo()
-//getaddrinfo(0, "5353", <#T##UnsafePointer<addrinfo>!#>, <#T##UnsafeMutablePointer<UnsafeMutablePointer<addrinfo>?>!#>)
-
-
-
-
-//query.pack().withUnsafeBufferPointer { p1 in
-//    sendto(socketfd, p1.baseAddress, p1.endIndex, 0, UnsafePointer(&dest_addr), socklen_t(sizeof(sockaddr_in)))
+//info.ai_protocol = IPPROTO_UDP
+//var result: UnsafeMutablePointer<addrinfo>? = nil
+//if(getaddrinfo(host, port, &info, &result) != 0) {
+//    abort()
 //}
-
-
-//let port = atoi
-
-//print(query.pack())
-
-//let socket = ActiveSocketIPv4()!
-//
-//socket.connect(address: "localhost:1337") {
-//    $0.write("Ring, ring!\r\n")
+//var res = result
+//while res != nil {
+//    if res?.pointee.ai_family == AF_INET {
+//        var buffer = UnsafeMutablePointer<CChar>.allocate(capacity: Int(INET_ADDRSTRLEN))
+//        var sa = res!.pointee.ai_addr.withMemoryRebound(to: sockaddr_in.self, capacity: 1) {
+//            return $0.pointee
+//        }
+//        if inet_ntop(res!.pointee.ai_family,
+//                     &sa.sin_addr, buffer, UInt32(INET_ADDRSTRLEN)) != nil {
+//            let ipAddress = String(validatingUTF8: buffer)
+//            print("IPv4 \(ipAddress) for host \(host):\(port)")
+//        }
+//    } else if res!.pointee.ai_family == AF_INET6 {
+//        var buffer = UnsafeMutablePointer<Int8>.allocate(capacity: Int(INET6_ADDRSTRLEN))
+//        var sa = res!.pointee.ai_addr.withMemoryRebound(to: sockaddr_in6.self, capacity: 1) {
+//            return $0.pointee
+//        }
+//        if inet_ntop(res!.pointee.ai_family,
+//                     &sa.sin6_addr, buffer, UInt32(INET6_ADDRSTRLEN)) != nil {
+//            let ipAddress = String(validatingUTF8: buffer)
+//            print("IPv6 \(ipAddress) for host \(host):\(port)")
+//        }
+//    }
+//    res = res!.pointee.ai_next
 //}
+//
+//freeaddrinfo(result)
 
-//print(socket.read())
-//print(socket.read())
+//print(info)
+//print(result?.pointee.ai_addr)
 
-import Foundation
-
-var input: InputStream?
-var output: OutputStream?
-
-Stream.getStreamsToHost(withName: "10.0.1.1", port: 53, inputStream: &input, outputStream: &output)
+// tcp
 //
-input!.open()
-output!.open()
+//import Foundation
 //
-////input.÷
+//var input: InputStream?
+//var output: OutputStream?
 //
+//Stream.getStreamsToHost(withName: "10.0.1.1", port: 53, inputStream: &input, outputStream: &output)
 //
+//input!.open()
+//output!.open()
 //
-////[0000]   00 24 82 F1 01 00 00 01   00 00 00 00 00 00 0E 67   .$...... .......g
-////[0010]   6F 6F 6F 6F 6F 6F 6F 6F   6F 6F 67 6C 65 03 63 6F   oooooooo oogle.co
-////[0020]   6D 00 00 01 00 01                                   m.....
+//var writeBuffer = query.tcp()
+//Data(bytes: query.tcp()).dump()
+//assert(output!.write(&writeBuffer, maxLength: writeBuffer.count) == writeBuffer.count)
 //
-////var writeBuffer = Array("Hello\n".utf8)
-var writeBuffer = query.tcp()
-Data(bytes: query.tcp()).dump()
-assert(output!.write(&writeBuffer, maxLength: writeBuffer.count) == writeBuffer.count)
-////
-////print(query.tcp())
-////
-////
-//sleep(1)
-usleep(20_000)
-while input!.hasBytesAvailable {
-    var readBuffer = Data(count: 1024)
-    readBuffer.count = readBuffer.withUnsafeMutableBytes { bytes in
-        input!.read(bytes, maxLength: readBuffer.count)
-    }
-    readBuffer.dump()
-    print(Message(unpack: readBuffer))
-}
+//usleep(30_000)
+//while input!.hasBytesAvailable {
+//    var readBuffer = Data(count: 1024)
+//    readBuffer.count = readBuffer.withUnsafeMutableBytes { bytes in
+//        input!.read(bytes, maxLength: readBuffer.count)
+//    }
+//    readBuffer.dump()
+//    print(Message(unpack: readBuffer))
+//}
