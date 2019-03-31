a391b61d3d1973179a85459e104c356af188a8b6
Bouke Haarsma <bouke@haarsma.eu>
2019-03-31 12:18:59 +0200

    Upgrade to 4.2 SwiftPM manifest

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 
 import PackageDescription
 
@@ -9,13 +9,12 @@ let package = Package(
         .executable(name: "dns-sd", targets: ["dns-sd"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Bouke/Cdns_sd.git", from: "1.0.0"),
+        .package(url: "https://github.com/Bouke/Cdns_sd.git", from: "2.0.0"),
         .package(url: "https://github.com/apple/swift-package-manager.git", from: "0.3.0"),
     ],
     targets: [
         .target(name: "NetService", dependencies: []),
         .target(name: "dns-sd", dependencies: ["NetService", "Utility"]),
         .testTarget(name: "NetServiceTests", dependencies: ["NetService"]),
-    ],
-    swiftLanguageVersions: [4]
+    ]
 )
