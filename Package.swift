3a95142261e0bcbc0d72778ce0eec905719506ac
Bouke Haarsma <bouke@haarsma.eu>
2020-08-26 21:16:39 +0200

    Internalise dns_sd library package

--- Package.swift
+++ Package.swift
@@ -1,19 +1,28 @@
-// swift-tools-version:4.2
+// swift-tools-version:5.1
 
 import PackageDescription
 
-let package = Package(
+var package = Package(
     name: "NetService",
     products: [
         .library(name: "NetService", targets: ["NetService"]),
         .executable(name: "dns-sd", targets: ["dns-sd"])
     ],
-    dependencies: [
-        .package(url: "https://github.com/Bouke/Cdns_sd.git", from: "2.0.0"),
-    ],
     targets: [
-        .target(name: "NetService", dependencies: []),
+        .target(name: "NetService", dependencies: ["Cdns_sd"]),
         .target(name: "dns-sd", dependencies: ["NetService"]),
         .testTarget(name: "NetServiceTests", dependencies: ["NetService"]),
     ]
 )
+
+#if !os(Linux)
+    package.targets.append(
+        .systemLibrary(name: "Cdns_sd"))
+#else
+    package.targets.append(
+        .systemLibrary(name: "Cdns_sd",
+                       pkgConfig: "avahi-compat-libdns_sd",
+                       providers: [
+                           .apt(["libavahi-compat-libdnssd-dev"])
+                       ]))
+#endif
